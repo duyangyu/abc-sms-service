6c2173b0fe7c00707e72501e82b4eb1a789b4972
Liang Guo <git@lkuo.me>
2018-04-24 19:57:22 -0400

    LG - add recipients

---
+++ src/main/java/org/theabconline/smsservice/config/Recipients.java
@@ -0,0 +1,16 @@
+package org.theabconline.smsservice.config;
+
+import java.util.List;
+
+public class Recipients {
+
+    private List<Recipient> recipients;
+
+    public List<Recipient> getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(List<Recipient> recipients) {
+        this.recipients = recipients;
+    }
+}
