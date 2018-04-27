f6a2df65489e1d6b819768b3c9751b2263449a07
Liang Guo <git@lkuo.me>
2018-04-27 00:38:52 -0400

    LG - implement log service

--- src/main/java/org/theabconline/smsservice/rest/TestResource.java
+++ src/main/java/org/theabconline/smsservice/rest/TestResource.java
@@ -5,18 +5,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.theabconline.smsservice.dto.SmsDTO;
+import org.theabconline.smsservice.dto.SmsExceptionDTO;
 import org.theabconline.smsservice.service.EmailService;
+import org.theabconline.smsservice.service.LogService;
 
 @RequestMapping("/api")
 @RestController
 public class TestResource {
 
     @Autowired
-    EmailService emailService;
+    LogService logService;
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public ResponseEntity sendMessage() {
-        emailService.send("test email", "lalalala");
+        logService.logFailure(new SmsExceptionDTO(new SmsDTO("phone number", "template code", "params"), "error message"));
 
         return ResponseEntity.ok().build();
     }
--- src/main/java/org/theabconline/smsservice/service/LogService.java
+++ src/main/java/org/theabconline/smsservice/service/LogService.java
@@ -3,10 +3,39 @@ package org.theabconline.smsservice.service;
 import org.springframework.stereotype.Service;
 import org.theabconline.smsservice.dto.SmsExceptionDTO;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 @Service
 public class LogService {
 
+    private static final String LOG_FILE = "/root/workspace/abc-sms-service/FailedMessages.csv";
+
     public void logFailure(SmsExceptionDTO smsExceptionDTO) {
+        FileWriter fileWriter = null;
+
+        try {
+            File logFile = new File(LOG_FILE);
+            fileWriter = new FileWriter(logFile, true);
+            fileWriter.append(smsExceptionDTO.getPhoneNumber()).append(";")
+                    .append(smsExceptionDTO.getTemplateCode()).append(";")
+                    .append(smsExceptionDTO.getParams()).append(";")
+                    .append(smsExceptionDTO.getErrorMessage())
+                    .append("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileWriter == null) {
+                return;
+            }
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
