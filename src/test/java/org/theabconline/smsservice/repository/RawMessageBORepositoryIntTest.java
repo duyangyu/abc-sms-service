a4b2b482c908986b966519cfca6dbfce352c6056
Liang Guo <git@lkuo.me>
2018-07-27 01:18:04 -0400

    LG - add test for RawMessageRepository

--- src/test/java/org/theabconline/smsservice/repository/RawMessageBORepositoryIntTest.java
+++ src/test/java/org/theabconline/smsservice/repository/RawMessageBORepositoryIntTest.java
@@ -1,13 +1,16 @@
 package org.theabconline.smsservice.repository;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 import org.theabconline.smsservice.entity.RawMessageBO;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -20,6 +23,20 @@ public class RawMessageBORepositoryIntTest {
 
     @Autowired
     private RawMessageRepository fixture;
+
+    @Test
+    public void testSave() {
+        Date createdOn = new Date();
+        RawMessageBO bo = new RawMessageBO();
+        bo.setMessage("message");
+        bo.setProcessed(false);
+        bo.setCreatedOn(createdOn);
+
+        fixture.save(bo);
+
+        List<RawMessageBO> boList = Lists.newArrayList(fixture.findAll());
+        assertEquals(3, boList.size());
+    }
 
     @Test
     public void testGetRawMessageBOSByIsProcessedFalse() {
