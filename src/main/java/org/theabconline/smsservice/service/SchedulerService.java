6e81f9744d4e06f5b1b6919d581d1648eeb10fd2
Liang Guo <git@lkuo.me>
2018-08-12 00:22:58 -0400

    LG - add back scheduled tasks

--- src/main/java/org/theabconline/smsservice/service/SchedulerService.java
+++ src/main/java/org/theabconline/smsservice/service/SchedulerService.java
@@ -12,12 +12,15 @@ public class SchedulerService {
 
     private final RecordService recordService;
 
+    private final SmsRequestService smsRequestService;
+
     @Value("${process.maxCount:3}")
     Integer maxCount;
 
     @Autowired
-    public SchedulerService(RecordService recordService) {
+    public SchedulerService(RecordService recordService, SmsRequestService smsRequestService) {
         this.recordService = recordService;
+        this.smsRequestService = smsRequestService;
     }
 
     @Scheduled(fixedDelayString = "${process.fixedDelay:5000}", initialDelay = 0)
@@ -33,15 +36,18 @@ public class SchedulerService {
     @Scheduled(fixedDelayString = "${process.fixedDelay:5000}", initialDelayString = "${process.fixedDelay:5000}")
     public void updateStatusAfterTenMinutes() {
         Long millis = 1000L * 60 * 10;
+        smsRequestService.updateRequestStatus(maxCount, millis);
     }
 
     @Scheduled(fixedDelayString = "${process.fixedDelay:5000}", initialDelayString = "${process.fixedDelay:5000}")
     public void updateStatusAfterOneHour() {
         Long millis = 1000L * 60 * 60;
+        smsRequestService.updateRequestStatus(maxCount, millis);
     }
 
     @Scheduled(fixedDelayString = "${process.fixedDelay:5000}", initialDelayString = "${process.fixedDelay:5000}")
     public void updateStatusAfterOneDay() {
         Long millis = 1000L * 60 * 60 * 24;
+        smsRequestService.updateRequestStatus(maxCount, millis);
     }
 }
